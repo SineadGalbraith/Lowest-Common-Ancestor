8a1cd371cc2beecc8bf89431a15557018df60515
Sinéad Galbraith <SineadGalbraith@users.noreply.github.com>
2018-09-25 14:40:22 +0100

    Tests running with a bigger BST

--- LCATest.java
+++ LCATest.java
@@ -17,5 +17,34 @@ class LCATest {
 		tree.root.right = new Node(3);
 		
 		assertEquals(1, tree.findLCA(2, 3));
+		
+		tree = new LCA();
+		// Tree with multiple elements
+		//  		   1
+		// 			/ 	  \
+		//	  	   2   	   3
+		//   	  /       / \
+		//		 4       9   10
+		//	    / \     / \   
+		//	   5   6   11 13
+		//	  / \	  /
+		//   7   8   12
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.left.left = new Node(5);
+		tree.root.left.left.right = new Node(6);
+		tree.root.left.left.left.left = new Node(7);
+		tree.root.left.left.left.right = new Node(8);
+		tree.root.right.left = new Node(9);
+		tree.root.right.right = new Node(10);
+		tree.root.right.left.left = new Node(11);
+		tree.root.right.left.left.left = new Node(12);
+		tree.root.right.left.right = new Node(13);
+		
+		assertEquals(9, tree.findLCA(12, 13));
+		assertEquals(3, tree.findLCA(12, 10));
+		assertEquals(1, tree.findLCA(8, 12));
 	}
 }
