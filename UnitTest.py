8c47fc8f444265ce1ec57750b66d888316aa333b
Sinéad Galbraith <32834111+SineadGalbraith@users.noreply.github.com>
2018-10-23 15:28:51 +0100

    Delete UnitTest.py

--- UnitTest.py
+++
@@ -1,9 +0,0 @@
-from Node import Node
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-
-	def test_newNode(self):
-		node = Node(1, "first")
-		self.assertEquals(node.getKey(),1)
-		self.assertEquals(node.getVal(), "first")
