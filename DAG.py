c30b9969670d9bbb500e2f960c51ae03e76c8e4f
Sinéad Galbraith <SineadGalbraith@users.noreply.github.com>
2018-10-24 21:01:58 +0100

    Updated DAG with existsPath and returnNode (not working yet)

--- DAG.py
+++ DAG.py
@@ -3,6 +3,8 @@ Created on 12 Oct 2018
 
 @author: sgal
 '''
+from platform import node
+from Node import Node
 class DAG:
     
     digraph = [] 
@@ -22,5 +24,23 @@ class DAG:
                 node.setValue(newNode.getValue())
                 return
         self.digraph.append(newNode)
+        
+    def returnNode(self, nodeKey):
+        for node in self.digraph:
+            if(node.getKey() == nodeKey):
+                return node
+        return None
+    
+    def returnNodeByKey(self, nodeKey):
+        for node in self.digraph:
+            if node.key == nodeKey:
+                return node
+        return None
+    
+    def existsPathBetween(self,startKey,endKey):
+        node = self.returnNodeByKey(startKey)
+        if(node==None):
+            return False
+        return node.findIfPathExists(endKey)
         
         
