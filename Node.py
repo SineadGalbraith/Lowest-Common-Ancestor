42b61061369a5ee8684b3fe38fdd6e207728eb2a
Sinéad Galbraith <SineadGalbraith@users.noreply.github.com>
2018-10-26 20:55:36 +0100

    Working Node

--- Node.py
+++ Node.py
@@ -6,46 +6,46 @@ Created on 12 Oct 2018
 class Node:
     pointsTo = []
     left = None
-    right = None 
-    
+    right = None
+
     def __init__(self, value, key):
         self.value = value
-        self.key = key 
+        self.key = key
         self.left = None
         self.right = None
         self.pointsTo = []
-        
+
     def getKey(self):
         return self.key
-    
+
     def getValue(self):
         return self.value
-    
+
     def setValue(self,value):
         self.value = value
-        
+
     def getRight(self):
         return self.right
-    
+
     def getLeft(self):
         return self.left
-    
+
     def pointsToNode(self, newNode):
         self.pointsTo.append(newNode)
-        
+
     def returnNodesPointedTo(self, ):
         sent = ""
         for node in self.pointsTo:
             sent = sent + str(node.getKey()) + "\n"
-            
+
         return sent
-        
+
     def hasEdgeTo(self,newNodeKey):
         for node in self.pointsTo:
             if(node.getKey() == newNodeKey):
                 return True
         return False
-    
+
     def findIfPathExists(self, dstKey):
         if(self.getKey() == dstKey):
             return True
@@ -53,3 +53,4 @@ class Node:
             if(node.findIfPathExists(dstKey) == True):
                 return True
         return False
+        
